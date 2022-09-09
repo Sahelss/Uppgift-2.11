9da59e3fc759d816d771d81aa7d9a5703bab4239
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-09-09 14:33:37 +0200

    Add project files.

---
+++ Uppgift 2.11/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Uppgift_2_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur långt hoppade Elin och Alma i meter?");
+            string x = Console.ReadLine();
+            Console.WriteLine("Elin hoppade 2,5 meter längre än Alma");
+        }
+    }
+}
